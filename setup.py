ffd1ff878761f24b00a41a579baf018f49412c06
taha <shaikh.taha95@gmail.com>
2019-10-23 14:04:33 +0500

    added flask as dependency

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-restful-paginate',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='mtShaikh',
+    author_email='',
+    description=''
+)
